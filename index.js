c9b3014e2e5ae136735ec4b5b8c026b3cf36c4c4
Andriy Pylypets <a.pylypets95@gmail.com>
2019-10-31 15:13:05 +0100

    Added path to front-end part for heroku deploying

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const db = require('./db')
 const Router = require('./routes/router')
 
 const app = express()
 const apiPort = 3000
+
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -20,5 +23,8 @@ app.get('/', (req, res) => {
 
 app.use('/api', Router)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
